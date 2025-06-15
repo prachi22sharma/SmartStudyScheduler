99275f7a4d7a848f25fe126c5c2324cbc0ec676e
myportfolio <prachisharma1322@gmail.com>
2025-06-16 00:59:03 +0530

    did some changes

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+
+import os
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="prachi@2204",
-            database="smartstudy"
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("DB_NAME")
         )
         if conn.is_connected():
             return conn
